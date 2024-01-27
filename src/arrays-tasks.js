4951315b10f5735e15c40347e8914144fb1482b3
Tim <jasper7466@yandex.ru>
2024-01-28 01:09:25 +0500

    feat: implement "getFalsyValuesCount" function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -426,8 +426,11 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce(
+    (accumulator, value) => (value ? accumulator : accumulator + 1),
+    0
+  );
 }
 
 /**
