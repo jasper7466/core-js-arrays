aea5aac1ff286ab681ba37f23b61b3c527a7065d
Tim <jasper7466@yandex.ru>
2024-01-28 00:52:26 +0500

    feat: implement "generateOdds" function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -384,8 +384,12 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  if (len === 0) {
+    return [];
+  }
+
+  return Array.from({ length: len }, (val, index) => index * 2 + 1);
 }
 
 /**
