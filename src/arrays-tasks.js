f6abb30672dde1223e8373bf9811fa1dfd465bce
Tim <jasper7466@yandex.ru>
2024-01-28 01:17:55 +0500

    feat: implement "getIdentityMatrix" function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -451,8 +451,10 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (v1, row) =>
+    Array.from({ length: n }, (v2, col) => (col === row ? 1 : 0))
+  );
 }
 
 /**
